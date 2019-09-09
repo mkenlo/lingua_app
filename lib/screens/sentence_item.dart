f79ed413f565152f3d61270eabf9c611398eb523
Melanie <mel.lontsi@gmail.com>
2019-09-09 16:07:35 -0700

    designed UI for an item sentence

--- lib/screens/sentence_item.dart
+++ lib/screens/sentence_item.dart
@@ -1,1 +1,27 @@
-// view for item sentence
+import "package:flutter/material.dart";
+
+import "../models/sentence_model.dart";
+
+class SentenceItem extends StatelessWidget {
+  final Sentence _phrase;
+  final int _index;
+
+  SentenceItem(this._phrase, this._index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Padding(
+            child: Text("$_index",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).accentColor)),
+            padding: EdgeInsets.symmetric(horizontal: 8.0)),
+        Expanded(child: Text(_phrase.text)),
+        Text(_phrase.language.code)
+      ]),
+    );
+  }
+}

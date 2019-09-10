8b9ec6794bf1bab6cced47e9f445364559d5fac4
Melanie <mel.lontsi@gmail.com>
2019-09-09 19:13:55 -0700

    refactor sentence tile

--- lib/screens/sentence_item.dart
+++ lib/screens/sentence_item.dart
@@ -8,20 +8,36 @@ class SentenceItem extends StatelessWidget {
 
   SentenceItem(this._phrase, this._index);
 
+  Color _tileBackgroundColor(BuildContext context) {
+    if (_index % 2 == 0) return Theme.of(context).primaryColorLight;
+    return Color.fromRGBO(1, 1, 1, 0.0);
+  }
+
+  void _navigateToRecorder(BuildContext context) {
+    if (Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
+
+    Navigator.push(context,
+        MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+              elevation: 1.0,
+              title: Text("Recording")),
+          body: Text("...loading Recording page..."),
+          resizeToAvoidBottomPadding: false);
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        Padding(
-            child: Text("$_index",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).accentColor)),
-            padding: EdgeInsets.symmetric(horizontal: 8.0)),
-        Expanded(child: Text(_phrase.text)),
-        Text(_phrase.language.code)
-      ]),
-    );
+        color: _tileBackgroundColor(context),
+        child: ListTile(
+          title: Text(_phrase.text),
+          subtitle: Text(_phrase.language.code),
+          trailing: Icon(Icons.translate),
+          onTap: () => _navigateToRecorder(context),
+        ));
   }
 }

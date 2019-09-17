1c2a36580d4c055b35b0950b8b8576c9be958e61
Melanie <mel.lontsi@gmail.com>
2019-09-17 16:13:00 -0700

    remove commented code

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /*return Scaffold(
-      appBar: AppBar(
-        title: Text("Lingua"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-      )
-    );*/
     return SentenceListScreen();
   }
 }
